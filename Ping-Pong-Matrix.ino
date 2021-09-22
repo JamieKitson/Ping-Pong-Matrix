f29314e83461c295cfd0b59b1453e83eaf75f2ce
Jamie Kitson <github@kitten-x.com>
2021-09-22 22:18:30 +0100

    FastLed, Rainbow working

--- Ping-Pong-Matrix.ino
+++ Ping-Pong-Matrix.ino
@@ -1,37 +1,110 @@
+#include <Adafruit_GFX.h>
+#include <FastLED_NeoMatrix.h>
 #include <FastLED.h>
-#include <FastLED_GFX.h>
-#include <LEDMatrix.h>
 #include "font.h"
 
-// declare FastLED (matrix / LED strip)
-#define LED_PIN        15
-#define COLOR_ORDER    GRB
-#define CHIPSET        WS2812B
+const int PAD = 4;
+const int WIDTH = 12 + PAD;
+const int HEIGHT = 7;
 
-// declare matrix
-#define MATRIX_WIDTH   12 // width of matrix
-#define MATRIX_HEIGHT  7 // height of matrix
-#define MATRIX_TYPE    (MTX_MATRIX_BOTTOM + MTX_MATRIX_LEFT + MTX_MATRIX_COLUMNS + MTX_MATRIX_PROGRESSIVE) // matrix layout flags, add together as needed
+CHSV colour( 0, 255, 180);
+const int NUMMATRIX = WIDTH * HEIGHT;
+CRGB leds[NUMMATRIX];
 
-// create our matrix based on matrix definition
-cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> matrix;
+FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(leds,
+  WIDTH, 7,
+//  NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_BOTTOM     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE
+  );
+
+const uint16_t colors[] = {
+  matrix->Color(255, 0, 0), matrix->Color(0, 255, 0), matrix->Color(255, 255, 0), matrix->Color(0, 0, 255), matrix->Color(255, 0, 255), matrix->Color(0, 255, 255), matrix->Color(255, 255, 255)
+};
+
+#define arr_len( x )  ( sizeof( x ) / sizeof( *x ) ) // Calculation of Array Size;
+
+int pixelPerChar = 3; // Width of Standard Font Characters is 8X6 Pixels
+
+char msg[] = "This is a longer message."; // BLANK Message of Your Choice;
+int msgSize = ((sizeof(msg) / sizeof(char)) * (pixelPerChar + 1)); // + (2 * pixelPerChar); // CACULATE message length;
+
+uint16_t myRemapFnTopRight(uint16_t x, uint16_t y) {
+  // x = x + ((5 - y) / 2);
+  // return (7 * (WIDTH - 1 - x)) + (y % 7);  
+}
+
+uint16_t myRemapFnBottomLeft(uint16_t x, uint16_t y) {
+  x = x - PAD + ((7 - y) / 2);
+  return (7 * x) + ((6 - y) % 7);  
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  
-  // initial FastLED by using CRGB led source from our matrix class
-  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(matrix[0], matrix.Size());
-  FastLED.setBrightness(127);
-  FastLED.clear(true);
+  FastLED.addLeds<NEOPIXEL,15>(  leds, NUMMATRIX  ).setCorrection(TypicalLEDStrip);
+  matrix->begin();
+  matrix->setFont(&TomThumb);
+  //matrix->setTextSize(3);
+  matrix->setTextWrap(false);
+  matrix->setRemapFunction(myRemapFnBottomLeft);
+  matrix->setBrightness(100);
+  matrix->setTextColor(CRGB::White);
+  matrix->print(msg); // Set the Message String;
+  /*
+  matrix->print("eo"); // Set the Message String;
+  matrix->show();
+  delay(1000);
+// */
+}
 
-  matrix.begin();
-  matrix.setFont(&TomThumb);
-  matrix.print("eo");
-  matrix.show();
+int counter = 120;
+#define LED_BLACK    0
+const int HUE_JUMP = 30;
 
+void RainbowAltPixels(int offset)
+{
+    for(int j = offset; j < 7; j += 2){
+      int p = (i * 7) + j;
+      if ((p >= 0) && (p < NUMMATRIX))
+      leds[p]= colour;
+    }
+    colour.hue = (colour.hue + HUE_JUMP / 2) % 256;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  
+//  matrix->fillScreen(colour); // BLANK the Entire Screen;
+  for ( int i=0; i < WIDTH; i++){
+    /*
+    for(int j = 0; j < 4; j++){
+      int p = ((i / 2) * 7) + (j * 2) + (i %2);
+      if ((p >= 0) && (p < NUMMATRIX))
+      leds[p]= colour;
+    }
+    colour.hue = (colour.hue + HUE_JUMP / 2) % 256;
+    /* */
+    for(int j = 1; j < 7; j += 2){
+      int p = (i * 7) + j;
+      if ((p >= 0) && (p < NUMMATRIX))
+      leds[p]= colour;
+    }
+    colour.hue = (colour.hue + HUE_JUMP / 2) % 256;
+    for(int j = 0; j < 7; j += 2){
+      int p = (i * 7) + j;
+      if ((p >= 0) && (p < NUMMATRIX))
+      leds[p]= colour;
+    }
+    colour.hue = (colour.hue + HUE_JUMP / 2) % 256;
+    // */
+  }
+  
+  colour.hue = (colour.hue - (WIDTH * HUE_JUMP) + 10) % 256;
+  
+  matrix->setCursor(counter-- / 10, 6); // Set Starting Point for Text String;
+  // matrix->print(msg); // Set the Message String;
+  matrix->show();
+  if (counter / 10 < -msgSize)
+    counter = 120;
+  delay(50);
+  //colour.hue = (colour.hue+1)%256;
 }
