2031463789cba13b0e9a571656231b6f733f9876
Jamie Kitson <github@kitten-x.com>
2021-09-22 22:42:37 +0100

    Clean up

--- Ping-Pong-Matrix.ino
+++ Ping-Pong-Matrix.ino
@@ -3,12 +3,13 @@
 #include <FastLED.h>
 #include "font.h"
 
+// Padding to allow italic letters into "virtual" space
 const int PAD = 4;
 const int WIDTH = 12 + PAD;
 const int HEIGHT = 7;
+const int NUMMATRIX = WIDTH * HEIGHT;
 
 CHSV colour( 0, 255, 180);
-const int NUMMATRIX = WIDTH * HEIGHT;
 CRGB leds[NUMMATRIX];
 
 FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(leds,
@@ -18,16 +19,12 @@ FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(leds,
   NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE
   );
 
-const uint16_t colors[] = {
-  matrix->Color(255, 0, 0), matrix->Color(0, 255, 0), matrix->Color(255, 255, 0), matrix->Color(0, 0, 255), matrix->Color(255, 0, 255), matrix->Color(0, 255, 255), matrix->Color(255, 255, 255)
-};
-
 #define arr_len( x )  ( sizeof( x ) / sizeof( *x ) ) // Calculation of Array Size;
 
-int pixelPerChar = 3; // Width of Standard Font Characters is 8X6 Pixels
+int pixelPerChar = 3;
 
-char msg[] = "This is a longer message."; // BLANK Message of Your Choice;
-int msgSize = ((sizeof(msg) / sizeof(char)) * (pixelPerChar + 1)); // + (2 * pixelPerChar); // CACULATE message length;
+char msg[] = "This is a longer message.";
+int msgSize = arr_len(msg) * (pixelPerChar + 1);
 
 uint16_t myRemapFnTopRight(uint16_t x, uint16_t y) {
   // x = x + ((5 - y) / 2);
@@ -35,8 +32,8 @@ uint16_t myRemapFnTopRight(uint16_t x, uint16_t y) {
 }
 
 uint16_t myRemapFnBottomLeft(uint16_t x, uint16_t y) {
-  x = x - PAD + ((7 - y) / 2);
-  return (7 * x) + ((6 - y) % 7);  
+  x = x - PAD + ((HEIGHT - y) / 2);
+  return (HEIGHT * x) + (HEIGHT - 1 - y);
 }
 
 void setup() {
@@ -58,50 +55,38 @@ void setup() {
 
 int counter = 120;
 #define LED_BLACK    0
-const int HUE_JUMP = 30;
-
-void RainbowAltPixels(int offset)
+/*
+void RainbowAltPixels(int column, int offset)
 {
     for(int j = offset; j < 7; j += 2){
-      int p = (i * 7) + j;
+      int p = (column * 7) + j;
       if ((p >= 0) && (p < NUMMATRIX))
       leds[p]= colour;
     }
     colour.hue = (colour.hue + HUE_JUMP / 2) % 256;
 }
-
+*/
 void loop() {
 
+  const int HUE_JUMP = 20;
   
-//  matrix->fillScreen(colour); // BLANK the Entire Screen;
-  for ( int i=0; i < WIDTH; i++){
-    /*
-    for(int j = 0; j < 4; j++){
-      int p = ((i / 2) * 7) + (j * 2) + (i %2);
-      if ((p >= 0) && (p < NUMMATRIX))
-      leds[p]= colour;
+  for ( int column = 0; column < WIDTH; column++)
+  {
+    for (int offset = 1; offset >= 0; offset--)
+    {
+      for(int row = offset; row < HEIGHT; row += 2)
+      {
+        int p = (column * HEIGHT) + row;
+        leds[p]= colour;
+      }
+      colour.hue = (colour.hue + HUE_JUMP / 2) % 256;
     }
-    colour.hue = (colour.hue + HUE_JUMP / 2) % 256;
-    /* */
-    for(int j = 1; j < 7; j += 2){
-      int p = (i * 7) + j;
-      if ((p >= 0) && (p < NUMMATRIX))
-      leds[p]= colour;
-    }
-    colour.hue = (colour.hue + HUE_JUMP / 2) % 256;
-    for(int j = 0; j < 7; j += 2){
-      int p = (i * 7) + j;
-      if ((p >= 0) && (p < NUMMATRIX))
-      leds[p]= colour;
-    }
-    colour.hue = (colour.hue + HUE_JUMP / 2) % 256;
-    // */
   }
   
-  colour.hue = (colour.hue - (WIDTH * HUE_JUMP) + 10) % 256;
+  colour.hue = (colour.hue - (WIDTH * HUE_JUMP) + 1) % 256;
   
   matrix->setCursor(counter-- / 10, 6); // Set Starting Point for Text String;
-  // matrix->print(msg); // Set the Message String;
+  matrix->print(msg); // Set the Message String;
   matrix->show();
   if (counter / 10 < -msgSize)
     counter = 120;
