cd2b270a639e807258c5338639971000d86739d6
Jamie Kitson <github@kitten-x.com>
2021-09-23 21:58:08 +0100

    WebServer working

--- Ping-Pong-Matrix.ino
+++ Ping-Pong-Matrix.ino
@@ -2,7 +2,14 @@
 #include <FastLED_NeoMatrix.h>
 #include <FastLED.h>
 #include "font.h"
-#include <Fonts/Picopixel.h>
+// #include <Fonts/Picopixel.h>
+#include <WiFi.h> 
+#include <NTPClient.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+const char* ssid = "BT-9ZAKH5";           
+const char* password = "k3nRrqEgPmYRAR";
 
 const int PIN = 15;
 
@@ -28,6 +35,10 @@ FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(leds,
   NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE
   );
 
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+AsyncWebServer server(80);
+
 #define arr_len( x )  ( sizeof( x ) / sizeof( *x ) ) // Calculation of Array Size;
 
 const int pixelPerChar = 4;
@@ -43,8 +54,16 @@ uint16_t myRemapFnBottomLeft(uint16_t x, uint16_t y)
   x = x - PAD + ((HEIGHT - BALL_DIR - y) / 2);
   return (HEIGHT * x) + (HEIGHT - 1 - y);
 }
-
-void setup() {
+/*
+void handleGenericArgs() 
+{ //Handler
+message = “Number of args received:”;
+message += server.args();  
+}
+*/
+void setup() 
+{
+  WiFi.begin(ssid, password);
   FastLED.addLeds<NEOPIXEL, PIN>(  leds, NUMMATRIX  ).setCorrection(TypicalLEDStrip);
   matrix->begin();
   matrix->setFont(&TomThumb);
@@ -52,6 +71,19 @@ void setup() {
   matrix->setRemapFunction(myRemapFnBottomLeft);
   matrix->setBrightness(100);
   matrix->setTextColor(CRGB::White);
+  //server.on("/", handleGenericArgs);
+  //*
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+  { 
+     if (request->params() > 0)
+     {
+       AsyncWebParameter* p = request->getParam(0); 
+       message = p->value();
+     }
+     request->send(200, "text/html", "<form method=get><input type=text name=message><submit></form>");
+  });
+  // */
+  server.begin();
 }
 
 void SetRainbow()
@@ -76,12 +108,63 @@ void SetRainbow()
   colour.hue = (colour.hue - (WIDTH * HUE_JUMP) + HUE_SPEED) % 256;
 }
 
+bool isConnected = false;
+
 void loop() 
 {
 
   const int COLOUR_STEPS_TO_TEXT = 10;
 
   SetRainbow();
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    isConnected = false;
+    message = "WiFi: " + String(WiFi.status());
+  }
+  else if (!isConnected)
+  {
+    isConnected = true;
+    message = "Connected";
+  }
+  else
+  {
+    /*
+      WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    message = "";
+    while (client.connected()) {  // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        if (c == '\n')
+        {
+          client.println(F("HTTP/1.1 200 OK"));
+          client.println();
+          client.stop();
+        }
+        else
+          message = message + c;
+      }
+    }
+    client.stop();
+  }
+    /*
+    WiFiClient client = server.available();
+    if (client) {
+
+    if (client.connected()) {
+      Serial.println("Connected to client");
+    }
+
+    // close the connection:
+    client.stop();
+  }
+  /*
+    timeClient.update();
+    message = timeClient.getFormattedTime();
+    */
+  }
 
   matrix->setCursor(counter-- / COLOUR_STEPS_TO_TEXT, 6);
   matrix->print(message);
